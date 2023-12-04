6b05bed943de514079233d5828a1d6042bf69cee
Dimitri Rog <foss.enjoyer@protonmail.com>
2023-12-04 18:42:06 +0100

    d4p2 better

---
+++ advent/target/debug/deps/advent-86764877e6083454.d
@@ -0,0 +1,5 @@
+/home/axea/projects/advent/advent/target/debug/deps/advent-86764877e6083454: src/main.rs
+
+/home/axea/projects/advent/advent/target/debug/deps/advent-86764877e6083454.d: src/main.rs
+
+src/main.rs:
---
+++ advent/target/release/advent.d
@@ -0,0 +1,1 @@
+/home/axea/projects/advent/advent/target/release/advent: /home/axea/projects/advent/advent/src/main.rs
