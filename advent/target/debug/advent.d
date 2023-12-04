537d9b2120d2d6285f4790617d92d0a708b80cee
Dimitri Rog <foss.enjoyer@protonmail.com>
2023-12-04 14:03:15 +0100

    d4p2 done super slow

---
+++ advent/target/debug/advent.d
@@ -0,0 +1,1 @@
+/home/axea/projects/advent/advent/target/debug/advent: /home/axea/projects/advent/advent/src/main.rs
---
+++ advent/target/debug/deps/advent-1c7045c32bf0165d.d
@@ -0,0 +1,5 @@
+/home/axea/projects/advent/advent/target/debug/deps/advent-1c7045c32bf0165d: src/main.rs
+
+/home/axea/projects/advent/advent/target/debug/deps/advent-1c7045c32bf0165d.d: src/main.rs
+
+src/main.rs:
